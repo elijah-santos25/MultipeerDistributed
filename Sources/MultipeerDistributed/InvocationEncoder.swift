5b01866b80e2401ead0b466acf28e5bc96fe7785
elijah-santos25 <elijah.santos25@bcp.org>
2023-06-22 16:50:20 -0700

    Use mangled type name for encoding remote calls

--- Sources/MultipeerDistributed/InvocationEncoder.swift
+++ Sources/MultipeerDistributed/InvocationEncoder.swift
@@ -5,6 +5,9 @@ import Distributed
 
 /// Don't interact with this struct; it's only public due to protocol requirements.
 public struct InvocationEncoder: DistributedTargetInvocationEncoder {
+    fileprivate enum InvocationEncodingError: DistributedActorSystemError {
+        case noMangledName
+    }
     var container = RemoteCallContainer()
     public mutating func recordArgument<Value: SerializationRequirement>(
         _ argument: RemoteCallArgument<Value>
@@ -13,11 +16,17 @@ public struct InvocationEncoder: DistributedTargetInvocationEncoder {
     }
     
     public mutating func recordReturnType<R: SerializationRequirement>(_ type: R.Type) throws {
-        container.returnType = _typeName(type)
+        guard let mangledName = _mangledTypeName(type) else {
+            throw InvocationEncodingError.noMangledName
+        }
+        container.returnType = mangledName
     }
     
     public mutating func recordGenericSubstitution<T>(_ type: T.Type) throws {
-        container.genericSubstitutions.append(_typeName(type))
+        guard let mangledName = _mangledTypeName(type) else {
+            throw InvocationEncodingError.noMangledName
+        }
+        container.genericSubstitutions.append(mangledName)
     }
     
     public mutating func recordErrorType<E>(_ type: E.Type) throws where E: Error {
